4ebce64a14d343c99eb945c7b5a3b3add005823d
PELENIUM <i.d.chernyshov@yandex.ru>
2023-10-06 19:38:38 +0300

    stopped drawing unneded and created a chaikin package

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module visualizer
 
 go 1.21.1
 
-require (
-	github.com/gen2brain/raylib-go/raygui v0.0.0-20230928181314-dc2584151090 // indirect
-	github.com/gen2brain/raylib-go/raylib v0.0.0-20230928181314-dc2584151090 // indirect
-)
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20230928181314-dc2584151090
--- main.go
+++ main.go
@@ -2,47 +2,23 @@ package main
 
 import (
 	"image/color"
+	"math/rand"
+	"visualizer/chaikin"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 var (
 	points = []rl.Vector2{
-		{X: 40, Y: 40},
-		{X: 200, Y: 130},
-		{X: 300, Y: 360},
-		{X: 500, Y: 340},
+		{X: float32(rand.Intn(800)), Y: float32(rand.Intn(800))},
+		{X: float32(rand.Intn(800)), Y: float32(rand.Intn(800))},
+		{X: float32(rand.Intn(800)), Y: float32(rand.Intn(800))},
+		{X: float32(rand.Intn(800)), Y: float32(rand.Intn(800))},
 	}
 )
 
-func chaikinSmooth(points []rl.Vector2) []rl.Vector2 {
-	result := make([]rl.Vector2, 0)
-
-	result = append(result, points[0])
-	for i := 0; i < len(points)-1; i++ {
-		prev := points[i]
-		next := points[i+1]
-
-		a := rl.Vector2{
-			X: prev.X*3/4 + next.X*1/4,
-			Y: prev.Y*3/4 + next.Y*1/4,
-		}
-
-		b := rl.Vector2{
-			X: prev.X*1/4 + next.X*3/4,
-			Y: prev.Y*1/4 + next.Y*3/4,
-		}
-
-		result = append(result, a)
-		result = append(result, b)
-	}
-	result = append(result, points[len(points)-1])
-
-	return result
-}
-
 func main() {
-	divisionCount := 3
+	divisionCount := 7
 
 	rl.SetConfigFlags(rl.FlagWindowHighdpi | rl.FlagMsaa4xHint)
 
@@ -52,22 +28,21 @@ func main() {
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-
 		rl.ClearBackground(rl.Black)
 
-		// Линии между точками
-		for i := 0; i < len(points)-1; i++ {
-			rl.DrawLine(
-				int32(points[i].X), int32(points[i].Y),
-				int32(points[i+1].X), int32(points[i+1].Y),
-				color.RGBA{255, 255, 255, 100},
-			)
-		}
+		// Lines between points
+		// for i := 0; i < len(points)-1; i++ {
+		// 	rl.DrawLine(
+		// 		int32(points[i].X), int32(points[i].Y),
+		// 		int32(points[i+1].X), int32(points[i+1].Y),
+		// 		color.RGBA{255, 255, 255, 100},
+		// 	)
+		// }
 
-		// Сглаженные линии
+		// Smoothed lines
 		newPoints := points
 		for i := 0; i < divisionCount; i++ {
-			newPoints = chaikinSmooth(newPoints)
+			newPoints = chaikin.ChaikinSmooth(newPoints)
 		}
 		for i := 0; i < len(newPoints)-1; i++ {
 			rl.DrawLineEx(
@@ -78,21 +53,26 @@ func main() {
 			)
 		}
 
-		// Сглаженные точки
-		for _, point := range newPoints {
-			rl.DrawCircle(int32(point.X), int32(point.Y), 5, rl.Orange)
-		}
+		// Smoothed points
+		// for _, point := range newPoints {
+		// 	rl.DrawCircle(int32(point.X), int32(point.Y), 5, rl.Orange)
+		// }
 
-		// Базовые точки
-		for _, point := range points {
-			rl.DrawCircle(int32(point.X), int32(point.Y), 7, rl.Red)
-		}
+		// Basic points
+		// for _, point := range points {
+		// 	rl.DrawCircle(int32(point.X), int32(point.Y), 7, rl.Red)
+		// }
 
 		rl.EndDrawing()
 
 		divisionCount++
-		if divisionCount > 5 {
-			divisionCount = 0
+		if divisionCount > 7 {
+			divisionCount = 7
+			points = []rl.Vector2{}
+			n := rand.Intn(5) + 4
+			for i := 0; i < n; i++ {
+				points = append(points, rl.Vector2{X: float32(rand.Intn(800)), Y: float32(rand.Intn(800))})
+			}
 		}
 	}
 
