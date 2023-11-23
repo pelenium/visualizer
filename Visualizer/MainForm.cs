f2793b622dbfcc900bb4e3195a9bd160f3a6230b
PELENIUM <i.d.chernyshov@yandex.ru>
2023-11-23 19:21:45 +0300

    some changes

--- Visualizer/MainForm.cs
+++ Visualizer/MainForm.cs
@@ -47,53 +47,63 @@ namespace Visualizer
                     List<List<Point>> points2 = new List<List<Point>>();
                     List<List<Point>> points3 = new List<List<Point>>();
 
-                    bool shouldAddTo3rd = false;
-
+                    int current = 0;
                     if (worksheet != null)
                     {
                         try
                         {
-                            for (int row = 1; row <= worksheet.Dimension.Rows; row++)
+                            for (int row = 2; row <= worksheet.Dimension.Rows; row++)
                             {
-                                if (shouldAddTo3rd)
+                                if (worksheet.Cells[row, 1].Value?.ToString().Trim(' ') == "/")
                                 {
-                                    if (points3.Count == 0 || string.IsNullOrWhiteSpace(worksheet.Cells[row, 2].Value?.ToString()))
-                                    {
-                                        points3.Add(new List<Point>());
-                                    }
-                                    else
-                                    {
-                                        points3[points3.Count - 1].Add(new Point((int)float.Parse(worksheet.Cells[row, 1].Value?.ToString()), (int)float.Parse(worksheet.Cells[row, 2].Value?.ToString())));
-                                    }
+                                    Console.WriteLine("found");
+                                    Console.WriteLine("found");
+                                    Console.WriteLine("found");
+                                    current++;
+                                    continue;
                                 }
-                                else
+
+                                switch (current)
                                 {
-                                    if (!string.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value?.ToString()) && string.IsNullOrWhiteSpace(worksheet.Cells[row, 3].Value?.ToString()))
-                                    {
-                                        shouldAddTo3rd = true;
-                                        continue;
-                                    }
-                                    if (points1.Count == 0 || string.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value?.ToString()))
-                                    {
-                                        points1.Add(new List<Point>());
-                                    }
-                                    if (points2.Count == 0 || string.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value?.ToString()))
-                                    {
-                                        points2.Add(new List<Point>());
-                                    }
-                                    else
-                                    {
+                                    case 0:
+                                        if (points1.Count == 0 || string.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value?.ToString()))
+                                        {
+                                            points1.Add(new List<Point>());
+                                        }
+                                        break;
+                                    case 1:
+                                        if (points2.Count == 0 || string.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value?.ToString()))
+                                        {
+                                            points2.Add(new List<Point>());
+                                        }
+                                        break;
+                                    case 2:
+                                        if (points3.Count == 0 || string.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value?.ToString()))
+                                        {
+                                            points3.Add(new List<Point>());
+                                        }
+                                        break;
+                                }
+                                switch (current)
+                                {
+                                    case 0:
                                         points1[points1.Count - 1].Add(new Point((int)float.Parse(worksheet.Cells[row, 1].Value?.ToString()), (int)float.Parse(worksheet.Cells[row, 2].Value?.ToString())));
-                                        points2[points2.Count - 1].Add(new Point((int)float.Parse(worksheet.Cells[row, 1].Value?.ToString()), (int)float.Parse(worksheet.Cells[row, 3].Value?.ToString())));
-                                    }
+                                        break;
+                                    case 1:
+                                        points2[points2.Count - 1].Add(new Point((int)float.Parse(worksheet.Cells[row, 1].Value?.ToString()), (int)float.Parse(worksheet.Cells[row, 2].Value?.ToString())));
+                                        break;
+                                    case 2:
+                                        points3[points3.Count - 1].Add(new Point((int)float.Parse(worksheet.Cells[row, 1].Value?.ToString()), (int)float.Parse(worksheet.Cells[row, 2].Value?.ToString())));
+                                        break;
                                 }
-                            }
-                        }
-                        catch (Exception ex1)
-                        {
-                            Console.WriteLine(ex1);
-                        }
+
                     }
+                }
+                    catch (Exception ex1)
+                    {
+                        Console.WriteLine(ex1);
+                    }
+                }
                     else
                     {
                         MessageBox.Show(
@@ -107,26 +117,23 @@ namespace Visualizer
                     }
 
                     points1.RemoveAll(i => i.Count == 0);
-                    points2.RemoveAll(i => i.Count == 0);
-                    // points3.RemoveAll(i => i.Count == 0);
 
                     var graphicResult1 = drawPoints(points1, true);
                     var graphicResult2 = drawPoints(points2, false);
-                    // var graphicResult3 = drawPoints(points3, false);
-
+                    var graphicResult3 = drawPoints(points3, false);
                     
 
                     graphicResult1.graphic.Save("result1.png");
-                    graphicResult2.graphic.Save("result2.png");
-                    // graphicResult3.graphic.Save("result3.png");
+                    graphicResult1.graphic.Save("result2.png");
+                    graphicResult1.graphic.Save("result3.png");
 
                     var img1 = Image.FromFile("result1.png");
                     var img2 = Image.FromFile("result2.png");
-                    // var img3 = Image.FromFile("result3.png");
+                    var img3 = Image.FromFile("result3.png");
 
                     graphic.DrawImage(img1, new Point(0, 0));
                     graphic.DrawImage(img2, new Point(0, 0));
-                    // graphic.DrawImage(img3, new Point(0, 0));
+                    graphic.DrawImage(img3, new Point(0, 0));
 
                     ResultGraphic.Image = graphicResult;
                 }
